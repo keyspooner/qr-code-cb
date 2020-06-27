a1ffc1b4b873e289024c732b981d54296242a727
Christopher Spooner <keyspooner@gmail.com>
2020-06-27 08:48:44 -0500

    Taking out test code so LIMS can handle click events, fixed styling, added submit button

--- cameraInit.js
+++ cameraInit.js
@@ -1,18 +1,53 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "environment" }, audio: false };
-// Define constants
-// const cameraView = document.querySelector("#camera--view"),
-//   cameraOutput = document.querySelector("#camera--output"),
-//   cameraSensor = document.querySelector("#camera--sensor"),
-//   cameraTrigger = document.querySelector("#camera--trigger");
 
-  //Initialize the Html5Qrcode class
-  const html5QrCode = new Html5Qrcode("camera");
+//Initialize the Html5Qrcode class
+const html5QrCode = new Html5Qrcode("camera");
 
-  function previewFile(node) {
-    const preview = $(node).parents(".card-body").find(".img-upload")[0]
-    const file = node.files[0];
-    const reader = new FileReader();
+//Add event listener for the camera button to toggle qr-code scanning on/off
+document.getElementById("scanSample").onclick = function () {
+  var x = document.getElementById("camera");
+  if (x.style.display === "none") {
+    //Start camera
+    cameraStart();
+    x.style.display = "block";
+  } else {
+    //Stop the camera
+    html5QrCode.stop();
+    x.style.display = "none";
+  }
+}
+
+//Add event listener for the image button to initiate look up of available SAMPLE_IMAGE tests
+document.getElementById("uploadImage").onclick = function () {
+  //check if we even have a value in the sample input
+  let sampleId = document.getElementById("sampleId").value;
+  //alert('sample id' + sampleId);
+  if (sampleId.trim() == '') {
+    alert("Please Scan or Enter a Sample ID");
+  } else {
+    //Determine how many images results need to be displayed
+
+    //TESTING
+    var imageDiv = document.getElementById("imagesToUpload");
+    $("<div class='row image'><div class='col'><div class='card'><div class='card-body'><div class='card-text text-right' onclick='deletePreviewFile(this)'><smallclass='text-muted'><i class='fas fa-trash'></i></small></div><div class='card-text text-left'><input type='file' accept='image/*' onchange='previewFile(this)'></div><img class='img-upload card-img-top' src='#' alt='Card image cap'></div></div></div></div>").appendTo(imageDiv);
+  }
+} 
+
+//Add event listener for the submit button to initiate pushing the base 64 data to LIMS
+document.getElementById("submitImages").onclick = function () {
+  //Find number of image children
+  let imageCount = document.getElementById("imagesToUpload").childElementCount;
+  alert(imageCount);
+} 
+
+
+//Function called on the onChange event for file input
+//Function finds the closest img element, gets the uploaded file, and sets it as the img source
+function previewFile(node) {
+  const preview = $(node).parents(".card-body").find(".img-upload")[0]
+  const file = node.files[0];
+  const reader = new FileReader();
   
     reader.addEventListener("load", function () {
       // convert image file to base64 string
@@ -25,68 +60,27 @@ var constraints = { video: { facingMode: "environment" }, audio: false };
     }
   }
 
-// $("input[type=file]").change(function(){
-//     alert($(this).val(""));
-//     const reader = new FileReader();
-//     reader.addEventListener("load", function () {
-//       //     // convert image file to base64 string
-//       //     //preview.src = reader.result;
-    
-//       //   }, false);
-      
-//       //   if (file) {
-//       //     reader.readAsDataURL(file);
-//       //   }
 
-//     });
-// })
-
-
-
-
-document.getElementById("scanSample").onclick = function () {
-  var x = document.getElementById("camera");
-  if (x.style.display === "none") {
-    cameraStart();
-    x.style.display = "block";
-  } else {
-    //How can I stop the camera when they toggle it off
-    html5QrCode.stop();
-    x.style.display = "none";
-  }
-}
-
-//delete image 
+//Function called on the onClick event for the div that surrounds the trash icon
+//Function finds the closest img element and clears the img source
 function deletePreviewFile(obj) {
   var image = $(obj).parents(".card-body").find("img").attr("src", "");
-  //var image = $(obj).parent().prop('className');//.find('img').attr('src');
-  console.log(image);
+  var fileInput = $(obj).parents(".card-body").find("input[type=file]").val("");
 }
-
-document.getElementById("uploadImage").onclick = function () {
-  //check if we even have a value in the sample input
-  let sampleId = document.getElementById("sampleId").value;
-  alert('sample id' + sampleId);
-  if (sampleId.trim() == '') {
-    alert("Please scan or enter a sample Id");
-  } else {
-    //now we have show a bunch of file inputs for images
-  }
-} 
 
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
   navigator.mediaDevices
-    .getUserMedia(constraints)
+    .getUserMedia(constraints) //Get the camera based on the constraints set above
     .then(function (stream) {
       const tracks = stream.getVideoTracks();
-      const deviceId = tracks.length && tracks[0].getCapabilities().deviceId;
+      const deviceId = tracks.length && tracks[0].getCapabilities().deviceId; //Assume they have only one environment facing camera
       const cameraToUse = deviceId;
 
-      //if we found an acceptable camera to use, start the camera
+      //If we found an acceptable camera to use, start the camera
       if (cameraToUse !== 0) {
-        alert("starting the camera" + cameraToUse);
+        //alert("starting the camera" + cameraToUse);
         html5QrCode.start(
           cameraToUse,     // retreived in the previous step.
           {
@@ -95,16 +89,11 @@ function cameraStart() {
             // scannable, rest shaded.
           },
           qrCodeMessage => {
-            // do something when code is read. For example:
-            // console.log(`QR Code detected: ${qrCodeMessage}`);
+            //Once we get a successful scan, stop and hide the camera
             html5QrCode.stop();
-            alert('scanned code is ' + qrCodeMessage);
-            document.getElementById("sampleId").value = qrCodeMessage;
             document.getElementById("camera").style.display = "none";
-
-            
-            
-
+            //Update the Sample ID input field with the scanned code
+            document.getElementById("sampleId").value = qrCodeMessage;
           },
           errorMessage => {
             // parse error, ideally ignore it. For example:
@@ -117,10 +106,6 @@ function cameraStart() {
       } else {
         alert("Could not find videoinput device")
       }
-
-      // track = stream.getTracks()[0];
-      // cameraView.srcObject = stream;
-
     })
     .catch(function (error) {
       console.error("Oops. Something is broken.", error);
